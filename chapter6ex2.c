2f7703d47c824f77938811af63ab9cbe54e6a519
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-20 12:34:13 -0500

    Create chapter6ex2.c

---
+++ chapter6ex2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+// this ones gives  a letter depending of the value you introduce, i used only if for a simple program
+int cal;
+int es;
+
+int main(void){
+ printf("enter value\n");
+ scanf(" %d", &cal);
+
+ if (cal < 60) {
+   printf("F");
+ }
+
+ if (cal > 61){
+   if (cal < 70)
+  printf("D");}
+
+  if (cal > 71){
+    if (cal < 80)
+    printf("C");
+  }
+
+ if (cal > 81){
+   if (cal < 90)
+   printf("B");
+ }
+ 
+  if (cal > 91){
+    if (cal < 100)
+    printf("A");
+  }
+
+return 0;
+}
