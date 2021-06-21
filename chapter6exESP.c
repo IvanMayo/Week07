51815e6041328980c17bc17c8c95d132170f3e05
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-20 22:27:11 -0500

    Create chapter6exESP.c

---
+++ chapter6exESP.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+// The price of corn: from 1 to 300 kg you pay 100$   if you buy more than 300 kg the price decreases to 90$  
+
+int corn;
+int cornprice= 100;
+int total;
+
+int main(void){
+  printf("insert the kilograms of corn \n");
+  scanf("%d", &corn);
+
+
+
+  if (corn >= 1 && corn <= 300 ){
+    total = corn * cornprice;
+
+    printf("the price of the corn will be %d", total);
+  }
+
+  else if (corn > 300){
+    total = corn * 90;
+    printf("the price of the corn will be %d", total);
+  }
+
+ 
+  return 0;
+}
