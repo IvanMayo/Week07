8d32a2106273afcf94bb85c2fe29afcbfaed4b0a
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-20 23:14:30 -0500

    Create chapter7ex1.c

---
+++ chapter7ex1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+// convert english units to metric units, it does not specify how many unit so I just did 3
+//investigate the convertions and then just apply them
+
+float miles, gallon, kelvin;
+
+
+float kilometer, liter, celcius;
+
+
+int main(void){
+  printf("enter miles \n");
+  scanf("%f", &miles);  
+ 
+  printf("enter gallons \n");
+  scanf("%f", &gallon);
+
+  printf("enter kelvin \n");
+  scanf("%f", &kelvin);
+
+  kilometer = miles* 1.609;
+  liter = gallon* 3.79;
+  celcius = kelvin - 273.1;
+
+  printf("in the metric system this will be \n %.2f  kilometers\n %.2f liters \n %.2f celcius", kilometer, liter, celcius);
+
+
+  return 0;
+}
