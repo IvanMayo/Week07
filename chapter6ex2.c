15fd5e7f41a77a688bc3bae468af5a427082339b
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-23 12:35:27 -0500

    Update chapter6ex2.c

--- chapter6ex2.c
+++ chapter6ex2.c
@@ -3,31 +3,33 @@
 // this ones gives  a letter depending of the value you introduce, i used only if for a simple program
 int cal;
 int es;
+char line [20];
 
 int main(void){
  printf("enter value\n");
- scanf(" %d", &cal);
+ fgets (line, sizeof(line), stdin);
+ sscanf(line, " %d", &cal);
 
- if (cal < 60) {
+ if (cal <= 60) {
    printf("F");
  }
 
- if (cal > 61){
-   if (cal < 70)
+ if (cal >= 61){
+   if (cal <= 70)
   printf("D");}
 
-  if (cal > 71){
-    if (cal < 80)
+  if (cal >= 71){
+    if (cal <= 80)
     printf("C");
   }
 
- if (cal > 81){
-   if (cal < 90)
+ if (cal >= 81){
+   if (cal <= 90)
    printf("B");
  }
  
-  if (cal > 91){
-    if (cal < 100)
+  if (cal >= 91){
+    if (cal <= 100)
     printf("A");
   }
 
