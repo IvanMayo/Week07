d53c16b7a125ef483fe7b9ecf074380eeb7e5412
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-23 12:46:11 -0500

    Update chapter6ex5.c

--- chapter6ex5.c
+++ chapter6ex5.c
@@ -4,30 +4,31 @@
 
 
  int X;
-
+  char year [50];
 int main(void){
 
   printf("inserte año\n");
-  scanf("%d", &X);
+  fgets(year, sizeof(year), stdin);
+  sscanf(year, "%d", &X);
 
   if (X % 400 == 0){
   printf("%d", X);
-  printf(" this is a biciesto year");
+  printf(" this is a leap year");
   }
   else if(X % 100 ==0){
     printf("%d", X);
-    printf(" this is not a biciesto year");
+    printf(" this is not a leap year");
   } 
 
   else if(X % 4 ==0){
     printf("%d", X);
-    printf(" this is a biciesto year");
+    printf(" this is a leap year");
   }
 
 
   else{
     printf("%d", X);
-    printf(" this is not a biciesto year");
+    printf(" this is not a leap year");
   }
   return 0;
 }
