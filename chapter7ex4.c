50459343ecfee00a087cf503bf1b4ca8a46477f6
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-22 22:34:31 -0500

    Create chapter7ex4.c

---
+++ chapter7ex4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//Write a program to add an 8% sales tax to a given amount and round the result to the nearest penny
+//floats for my variables
+//the code is just a multiplication and a sum
+//tax = amount * Tax;
+//total = amount + tax;
+
+float amount;
+float tax;
+float total;
+float Tax = .08;
+
+
+int main(void)
+{
+       
+  printf("Enter the value of your item\n");
+  scanf("%f", &amount);
+  tax = amount * Tax;
+  total = amount + tax;
+ 
+printf("With sales tax your item costs is %.2f",total);
+
+ getchar(); //this rounds my value
+ return 0;
+}
