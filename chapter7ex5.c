c382550a5a174da300c8878a3b4bc8050eadb801
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-22 23:48:45 -0500

    Create chapter7ex5.c

---
+++ chapter7ex5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Write a program to tell if a number is prime
+
+
+
+
+int main (void)
+{
+  int num;
+  int X;
+  int prime;
+ 
+  printf("Enter a number \n");
+  scanf("%d", &num);
+ 
+  if (num <= 1)
+  {
+  printf("%d is not a prime numbers \n", num);
+  exit(1);
+  }
+
+  prime = 0;
+  
+  for (X = 2; X <= num / 2; X++)
+  {
+    if ((num % X) == 0){
+    prime = 1;
+    break; }
+  }
+    
+  if (prime == 0)
+    printf("the number %d is prime \n", num);
+  else
+    printf("the number %d is not prime \n", num);
+}
