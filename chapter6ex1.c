41b9cb67541219a170381512446d86756d05aea3
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-23 12:31:46 -0500

    Update chapter6ex1.c

--- chapter6ex1.c
+++ chapter6ex1.c
@@ -3,15 +3,19 @@
 // the square between the distance of two points
 float X1,  Y1;
 float X2,  Y2;
-float xt,  yt;
+char line [50];
+char line2 [50];
 float raiz;
 
 int main(void){
- X1 = 5;
- Y1 = 7;
+ printf("Enter x1 and y1\n");
+ fgets(line, sizeof(line), stdin);
+ sscanf(line, "%f %f", &X1, &Y1);
 
- X2 = 8;
- Y2 = 9;
+ printf("Enter x2 and y2\n");
+ fgets(line2, sizeof(line2), stdin);
+ sscanf(line2, "%f %f", &X2, &Y2);
+
 
 raiz = sqrt( ((X2-X1)*(X2-X1)) + ((Y2-Y1)*(Y2-Y1)) ) ;
 
