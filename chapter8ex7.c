39c16c00e277a477b181157461ebc4fbab17d5fa
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-23 21:21:25 -0500

    Create chapter8ex7.c

---
+++ chapter8ex7.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <string.h>
+
+int number;                 
+
+int tens_place;             
+int last_digit;             
+
+int main() {
+
+	for (number = 0; number <= 100; ++number) {
+
+		
+		if (number < 20) {
+			switch (number) {
+				case 0:
+					printf("zero");
+					break;
+				case 1:
+					printf("one");
+					break;
+				case 2:
+					printf("two");
+					break;
+				case 3:
+					printf("three");
+					break;
+				case 4:
+					printf("four");
+					break;
+				case 5:
+					printf("five");
+					break;
+				case 6:
+					printf("six");
+					break;
+				case 7:
+					printf("seven");
+					break;
+				case 8:
+					printf("eight");
+					break;
+				case 9:
+					printf("nine");
+					break;
+				case 10:
+					printf("ten");
+					break;
+				case 11:
+					printf("eleven");
+					break;
+				case 12:
+					printf("twelve");
+					break;
+				case 13:
+					printf("thirteen");
+					break;
+				case 14:
+					printf("fourteen");
+					break;
+				case 15:
+					printf("fifteen");
+					break;
+				case 16:
+					printf("sixteen");
+					break;
+				case 17:
+					printf("seventeen");
+					break;
+				case 18:
+					printf("eighteen");
+					break;
+				case 19:
+					printf("nineteen");
+					break;
+			}
+
+		} else if (number == 100) {
+			printf("one hundred");
+
+		} else {
+			tens_place = number / 10;     
+			last_digit = number % 10;       
+
+			switch (tens_place) {
+				case 2:
+					printf("twenty");
+					break;
+				case 3:
+					printf("thirty");
+					break;
+				case 4:
+					printf("forty");
+					break;
+				case 5:
+					printf("fifty");
+					break;
+				case 6:
+					printf("sixty");
+					break;
+				case 7:
+					printf("seventy");
+					break;
+				case 8:
+					printf("eighty");
+					break;
+				case 9:
+					printf("ninety");
+					break;
+			}
+
+			switch (last_digit) {
+				case 1:
+					printf("-one");
+					break;
+				case 2:
+					printf("-two");
+					break;
+				case 3:
+					printf("-three");
+					break;
+				case 4:
+					printf("-four");
+					break;
+				case 5:
+					printf("-five");
+					break;
+				case 6:
+					printf("-six");
+					break;
+				case 7:
+					printf("-seven");
+					break;
+				case 8:
+					printf("-eight");
+					break;
+				case 9:
+					printf("-nine");
+					break;
+			}
+		}
+
+		printf("\n");
+	}
+
+	return(0);
+}
